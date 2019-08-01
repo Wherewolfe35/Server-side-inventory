943ac9578cf8211f7a95c01b7ede04026984bb0e
Aaron Wolfe <3wolfeaaron5@gmail.com>
2019-07-31 21:11:52 -0500

    allows input to be appended to the DOM from server data

--- server/public/script/client.js
+++ server/public/script/client.js
@@ -2,16 +2,42 @@ $(document).ready(handlerReady);
 
 function handlerReady() {
   console.log('jquery is up and running');
-  $('#inputBtn').on('click', getItems);
+  $('#inputBtn').on('click', addItems);
 }
 
 function getItems() {
   console.log('getting items');
-  
   $.ajax({
     method: 'GET',
-    url: '/items'
-  }).then((response)=>{
+    url: '/items',
+  }).then((response) => {
     console.log('got items', response);
+    renderItems(response);
   })
 }
+
+function addItems() {
+  console.log('Posting Items');
+  let newItem = {
+    name: $('#newItem').val(),
+    description: $('#itemDescription').val()
+  }
+  $.ajax({
+    method: 'POST',
+    url: '/items',
+    data: newItem,
+  }).then((response)=>{
+    console.log('response posted ', response);
+    getItems();
+  })
+  $('#newItem').val('');
+  $('#itemDescription').val('');
+}
+
+function renderItems(items) {
+  $('#invList').empty(); // gets rid of old stuff
+  for (const item of items) {
+    $('#invList').append(`<li><img src="http://fc07.deviantart.net/fs42/f/2009/164/2/f/Free_Wolf_Icon_Template_by_RatRabbit.png">${item.name}: ${item.description}</li>`);
+  }
+}
+
